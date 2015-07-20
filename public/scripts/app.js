4a964f8bc058072476ea69d96fd7168fc02ac6b8
Steve <stephendwebster@gmail.com>
2015-07-19 21:25:21 -0700

    Create a Yeoman scaffold for front end

---
+++ public/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicYeoApp
+ * @description
+ * # publicYeoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicYeoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name publicYeoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the publicYeoApp
+ */
+angular.module('publicYeoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
